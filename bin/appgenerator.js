98aa4cbcfd81ad9550bc98f1af730d870352b5ae
sudaoyi <3303332004@qq.com>
2016-08-30 21:43:58 +0800

    add the touch method

--- bin/appgenerator.js
+++ bin/appgenerator.js
@@ -1,20 +1,20 @@
-#!/usr/bin/env node 
-const mn=require('../mn.js');
-const process=require('process');
-const path=require('path');
-const cfg=require('../config.js');
-var dirs=process.argv.slice(2);
+#!/usr/bin/env node
+const mn = require('../mn.js');
+const process = require('process');
+const path = require('path');
+const cfg = require('../config.js');
+var dirs = process.argv.slice(2);
 
-var dirlist=['bin','doc','test','model','views'];
-var npmlist={save:['suba','sui'],
-            'save-dev':['li','su']}
 
-if(dirs.length==0){
-   cfg.dirlist.forEach((v,i,a)=>{ mn.mkdir(v)});
-   mn.npmStart(cfg.npmlist)
-}else{	   
-   dirs.forEach((dv,di,da)=>{
-	cfg.dirlist.forEach((v,i,a)=>{mn.mkdir(path.join(dv,v))});
-	mn.npmStart(cfg.npmlist,dv)
-   })
+
+if (dirs.length == 0) {
+   cfg.dirlist  && cfg.dirlist.forEach((v, i, a) => { mn.mkdir(v) });
+   cfg.filelist && cfg.filelist.forEach((v, i, a) => { mn.touch(v) });
+    mn.npmStart(cfg.npmlist)
+} else {
+    dirs.forEach((dv, di, da) => {
+     cfg.dirlist  &&  cfg.dirlist.forEach((v, i, a) => { mn.mkdir(path.join(dv, v)) });
+     cfg.filelist &&  cfg.filelist.forEach((v, i, a) => { mn.touch(path.join(dv, v)) });
+        mn.npmStart(cfg.npmlist, dv)
+    })
 }
--- mn.js
+++ mn.js
@@ -1,42 +1,59 @@
-const fs=require('fs')
-const path=require('path')
-const exec=require('child_process').exec
-const execSync=require('child_process').execSync
-const process=require('process')
+const fs = require('fs')
+const path = require('path')
+const exec = require('child_process').exec
+const execSync = require('child_process').execSync
+const process = require('process')
 
 
-var mkdir=function(dir){
-	  exec('mkdir -p '+dir,(e,stdout,stderr)=>{
-			if(e){console.log(e);return};
-		});
-	 console.log(`${dir} is done`);
+var mkdir = function (dir) {
+	exec('mkdir -p ' + dir, (e, stdout, stderr) => {
+		if (e) { console.log(e); return };
+	});
+	console.log(`${dir} is done`);
+}
+
+var touch = function (file) {
+	var fileObj = path.parse(file);
+    var supDir=process.cwd();
+	if (fileObj.dir) {
+		try { execSync('mkdir -p ' + fileObj.dir) } catch (e) { console.log(e, file + ' fail to touch file'); return };
+		try { process.chdir('./' + fileObj.dir) } catch (e) { console.log(e, file + ' fail to touch file'); return };
+		try { execSync('touch ' + fileObj.base) } catch (e) { console.log(e, file + ' fail to touch file'); return };
+		try { process.chdir(supDir) } catch (e) { console.log(e); return }
+	} else {
+		try { execSync('touch ' + fileObj.base) } catch (e) { console.log(e, file + ' fail to touch file'); return };
 	}
+}
 
-var npminit=function(dlist){
-	try{ execSync('npm init -y')}catch(e){console.log(e);return}	
+var npminit = function (dlist) {
+	try { execSync('npm init -y') } catch (e) { console.log(e); return }
 	console.log(dlist);
 
-	if(dlist['save']){
-	   dlist['save'].forEach((v,i,a)=>{
-	   	    exec('npm install --save '+v,(e,stdout,stderr)=>{
-			   if(e){console.log(e);return};
+	if (dlist['save']) {
+		dlist['save'].forEach((v, i, a) => {
+			exec('npm install --save ' + v, (e, stdout, stderr) => {
+				if (e) { console.log(e); return };
 			});
-		})};
-	if(dlist['save-dev']){
-	   dlist['save-dev'].forEach((v,i,a)=>{
-	   	    exec('npm install --save-dev '+v,(e,stdout,stderr)=>{
-			   if(e){console.log(e);return};
+		})
+	};
+	if (dlist['save-dev']) {
+		dlist['save-dev'].forEach((v, i, a) => {
+			exec('npm install --save-dev ' + v, (e, stdout, stderr) => {
+				if (e) { console.log(e); return };
 			});
-		   });
-		}
+		});
 	}
-	
-var npmStart=function(dlist,dir){
-	if(dir){try{process.chdir(dir)}catch(e){console.log(e)}	  
-	  	npminit(dlist);
+}
 
-	   try{process.chdir('../')}catch(e){console.log(e)}	  
-	}else{
-		npminit(dlist);}
+var npmStart = function (dlist, dir) {
+
+	if (dir) {
+		try { process.chdir(dir) } catch (e) { console.log(e); return }
+		npminit(dlist);
+
+		try { process.chdir('../') } catch (e) { console.log(e); return }
+	} else {
+		npminit(dlist);
 	}
-module.exports={mkdir:mkdir,npmStart:npmStart};	
+}
+module.exports = { mkdir: mkdir, npmStart: npmStart, touch: touch };	
